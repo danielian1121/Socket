874329b1e10b04b33d4e0b6512b846b8879b5670
danielian1121 <danielian1121@gmail.com>
2018-08-26 22:42:43 +0800

    FUCK

--- server.js
+++ server.js
@@ -11,88 +11,12 @@ server.listen(8124, function () {
 io.listen(server);
 var serv_io = io.listen(server);
 serv_io.sockets.on('connection', function(socket) {
-  /*socket.on('lng_data', function(data) {
-    console.log("lng");
-    console.log(data);
-    socket.broadcast.emit('lng_data',data);
-  });
-  socket.on('lat_data', function(data) {
-    console.log("lat");
-    console.log(data);
-    socket.broadcast.emit('lat_data',data);
-  });
-  socket.on('type_data', function(data) {
-    console.log("type");
-    console.log(data);
-    socket.broadcast.emit('type_data',data);
-  });*/
-  socket.on('LOI_position', function(data) {
-    var mdata = data.toString();
-    console.log("LOI_position");
-    console.log(data);
-    socket.broadcast.emit('LoiGetPosition',mdata);
-  });
-  socket.on('AOI_position', function(data) {
-    var mdata = data.toString();
-    console.log("AOI_position");
-    console.log(data);
-    socket.broadcast.emit('AoiGetPosition',mdata);
-  });
-  socket.on('LoiShare', function(data) {
-    console.log("LoiShare");
-    let mdata = JSON.stringify(data);
-    console.log(mdata);
-    socket.broadcast.emit('LoiChange',mdata);
-  });
-  socket.on('position', function(data) {
-    var mdata = data.toString();
-    console.log("position");
-    console.log(data);
-    socket.broadcast.emit('GetPosition',mdata);
-  });
-  socket.on('share', function(data) {
-    console.log("share");
-    let mdata = JSON.stringify(data);
-    console.log(mdata);
-    socket.broadcast.emit('ChangeView',mdata);
-  });
-
-  socket.on('ios_search_group_id',function(data){
-    console.log("ios_search_group_id");
-    socket.broadcast.emit('ios_search_receive_group_id',data);
-  });
-  socket.on('ios_show_group_id',function(data){
-    console.log("ios_show_group_id");
-    socket.broadcast.emit('ios_show_receive_group_id',data);
-  });
-  socket.on('ios_send_longtitude', function(data) {
-    console.log("ios_send_longtitude");
-    socket.broadcast.emit('ios_receive_longtitude',data);
-  });
-  socket.on('ios_send_latitude', function(data) {
-    console.log("ios_send_latitude");
-    socket.broadcast.emit('ios_receive_latitude',data);
-  });
-socket.on('ios_send_type', function(data) {
-  console.log("ios_send_type");
-    socket.broadcast.emit('ios_receive_type',data);
-  });
-
-  socket.on('search',function(data){
-    let mdata = JSON.stringify(data);
-    console.log(mdata);
-    socket.broadcast.emit('get_search',mdata);
-  });
-
-
-socket.on('ios_send_poi', function(data) {
-    console.log('ios_send_poi')
-    console.log(data);
-    socket.broadcast.emit('ios_receive_poi',data);
-  });
-socket.on('ios_send_xoi', function(data) {
-  console.log('ios_send_xoi')
-    console.log(data);
-    socket.broadcast.emit('ios_receive_xoi',data);
+  // 傳送時間訊息給瀏覽器
+  setInterval(function() {
+    socket.emit('date', {'date': new Date()});
+  }, 10);
+  // 接收來自於瀏覽器的資料
+  socket.on('client_data', function(data) {
+    process.stdout.write(data.letter);
   });
 });
